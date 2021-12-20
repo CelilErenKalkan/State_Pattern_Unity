3212df71af4b356e289405cf3df93ef3a31cb2f9
Celil Eren Kalkan <60541083+CelilErenKalkan@users.noreply.github.com>
2021-12-20 13:22:44 +0300

    State Script Added
    The script of the State Pattern has been written. As well as the patrol mechanic and character AI.

---
+++ State_Pattern/Assets/GameEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Assets
+{
+    public sealed class GameEnvironment
+    {
+        private static GameEnvironment instance;
+        private List<GameObject> checkpointsList = new List<GameObject>();
+        public List<GameObject> CheckpointsList
+        {
+            get { return checkpointsList; }
+        }
+
+        public static GameEnvironment Singleton
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameEnvironment();
+                    instance.CheckpointsList.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+                    instance.checkpointsList = instance.checkpointsList.OrderBy(waypoint => waypoint.name).ToList();
+                }
+
+                return instance;
+            }
+        }
+
+        public GameObject RandomGoal()
+        {
+            return checkpointsList[Random.Range(0, checkpointsList.Count)];
+        }
+    }
+}
